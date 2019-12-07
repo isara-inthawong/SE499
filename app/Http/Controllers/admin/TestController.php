073489de7f517a9604def791fe078a74d33c334e
isara <bell7672@gmail.com>
2019-12-08 04:40:35 +0700

    fix bug create dummy data

--- app/Http/Controllers/admin/TestController.php
+++ app/Http/Controllers/admin/TestController.php
@@ -20,6 +20,9 @@ class TestController extends Controller
 
     public function create_dummy_user()
     {
+        if (session('success')) {
+            Alert::success(session('success'));
+        }
         $faker = Factory::create();
 
         for ($i = 0; $i < 100; $i++) {
@@ -30,7 +33,7 @@ class TestController extends Controller
             $num = $num[$num1];
             $major = $major[$major1];
 
-            User::create([
+            $userData = array(
                 'first_name' => $faker->firstName('male' | 'female'),
                 'last_name' => $faker->lastName,
                 'tel' => $num . $faker->ean8,
@@ -38,31 +41,40 @@ class TestController extends Controller
                 'email' => $faker->email,
                 'password' => Hash::make('qqqqwwww'),
                 'user_image' => 'profile-default.jpg',
-            ]);
+            );
+
+            $exists = User::where('email', '=', $userData['email'])->first();
+
+            if (!$exists) {
+                User::create($userData);
+            }
         }
-        Alert::toast('Create User Data Successfully', 'success');
-        return redirect('/admin/create_dummy_activity')->with('status', 'Please wait to create activity data.');
+        return redirect('admin/create_dummy_activity');
     }
 
     public function create_dummy_activity()
     {
-        if (session('status')) {
-            Alert::success(session('status'));
-        }
-
         $faker = Factory::create();
 
         for ($i = 0; $i < 50; $i++) {
-            Activity::create([
+
+            $activityData = array(
                 'activity_name' => $faker->userName,
                 'activity_address' => $faker->address,
                 'activity_date' => $faker->date($format = 'Y-m-d', $min = 'now'),
                 'activity_time' => $faker->time($format = 'H:i:s', $min = 'now'),
                 'hour' => $faker->randomDigit,
                 'activity_detail' => $faker->realText($maxNbChars = 200, $indexSize = 2),
-                'activity_image' => $faker->userName,
-            ]);
+                'activity_image' => 'banner-01.jpg',
+            );
+
+            $exists = Activity::where('activity_name', '=', $activityData['activity_name'])->first();
+
+            if (!$exists) {
+                Activity::create($activityData);
+            }
         }
-        Alert::toast('Create User Data Successfully', 'success');
+        Alert::success('Create Dummy Data Successfully', 'success');
+        return redirect('admin/home');
     }
 }
